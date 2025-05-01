c550a3a0c10b4b2b80571721f1201a52baa33d40
fantasyzhjk <fantasyzhjk@outlook.com>
2025-05-01 22:47:59 +0800

    精简依赖

--- SharpRosuPP/RosuPP/Rosu.cs
+++ SharpRosuPP/RosuPP/Rosu.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Text.Json.Serialization;
 using RosuPP;
 
 #nullable enable
